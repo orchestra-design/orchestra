66c1f644c766fbb6b62d9d156fcb6c786c0d480d
Alexei Accio <alexei.accio@gmail.com>
2018-06-26 15:35:46 +0300

    image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,41 @@
 /* global tw */
 import React from 'react'
-import styled from 'react-emotion'
+import styled, { css } from 'react-emotion'
+
+const dynamicStyle = props =>
+  css`
+    background-image: url(${props.src});
+  `
 
 const Page = styled('div')`
-  ${tw('flex justify-center items-center absolute pin bg-grey-light')};
-`;
+  ${tw('flex justify-center items-center absolute pin bg-grey-light bg-center bg-cover bg-no-repeat')};
+  ${dynamicStyle};
+`
 
 const Container = styled('div')`
   ${tw(
     'bg-white container my-8 mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden font-sans'
   )};
-`;
+`
 
 const Wrapper = styled('div')`
   ${tw('sm:flex sm:items-center px-6 py-4')};
-`;
+`
 
 const TextContainer = styled('div')`
   ${tw('text-center sm:text-left sm:flex-grow')};
-`;
+`
 
 
-export default ({ data }) => (
-  <Page>
+export default ({ data: { homepage: { data } } }) => (
+  <Page src={data.body[0].items[0].gallery_image.url}>
     <Container>
       <Wrapper>
         <TextContainer>
-        { data.homepage.data.underconstruction.text }
+        { data.underconstruction.text }
         </TextContainer>
-        {console.log(data)}
       </Wrapper>
+      {console.log(data.body[0].items[0].gallery_image.localFile.absolutePath)}
     </Container>
   </Page>
 )
@@ -52,6 +58,7 @@ export const query = graphql`
             gallery_image {
               url
               localFile {
+                absolutePath
                 relativePath
               }
             }
