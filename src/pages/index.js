4a2b6e750473d07c63105dbd2dacc2617dcdf782
Alexei Accio <alexei.accio@gmail.com>
2018-06-26 23:44:31 +0300

    buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,42 @@ import { lifecycle } from 'recompose'
 import LogoSvg from '../images/orchestra-logo.svg'
 
 const Container = styled('div')`
-  ${tw('flex justify-center absolute pin bg-black items-center w-screen h-screen')};
+  ${tw('flex flex-col justify-center absolute pin bg-black items-center w-screen h-screen')};
 `
 
 const Logo = styled('div')`
   ${tw('absolute pin-t pin-l bg-right-bottom bg-contain bg-no-repeat')};
-  width: calc(200px + 120 * ((100vw - 320px) / 1280));
+  width: calc(200px + 90 * ((100vw - 320px) / 1280));
   height: calc(56px + 34 * ((100vw - 320px) / 1280));
   background-image: url(${LogoSvg});
 `
 
 const TextContainer = styled('div')`
-  ${tw('text-center relative font-bold text-white z-30 sm:flex-grow')};
+  ${tw('text-center relative font-bold text-white z-20')};
   font-family: 'IBM Plex Sans', sans-serif;
   font-size: calc(18px + 36 * ((100vw - 320px) / 1280));
   text-shadow: 0px 0px 24px rgba(0, 0, 0, 1);
+`
+
+const Button = css`
+  ${tw('absolute flex justify-center align-center m-6 cursor-pointer no-underline text-white hover:text-black bg-black hover:bg-white shadow-none hover:shadow-md z-50')};
+  font-family: 'Source Sans Pro', sans-serif;
+  font-size: calc(12px + 4 * ((100vw - 320px) / 1280));
+  font-variant-caps: all-small-caps;
+  transition: all .2s ease-in-out;
+  white-space: nowrap;
+`
+
+const LangSwitcher = styled('span')`
+  ${tw('pin-r pin-t')}
+  ${Button};
+  padding: 1rem;
+`
+
+const Email = styled('a')`
+  ${tw('pin-r pin-b')}
+  ${Button};
+  padding: 1rem 1.5rem;
 `
 
 const withToggle = lifecycle({
@@ -75,19 +96,42 @@ const Slider = withToggle(({ slides, index, mount }) => {
 )})
 
 
-export default ({ data: { ru: { data } } }) => (
+export default ({ data }) => (
   <Container>
-    <Slider slides={data.slider} />
+    <Slider slides={data.ru.data.slider} />
     <Logo />
+    <LangSwitcher>en</LangSwitcher>
     <TextContainer>
-    { data.underconstruction.text }
+    { data.ru.data.underconstruction.text }
     </TextContainer>
+    <Email href={data.ru.data.email.url} >
+    { data.ru.data.email.url.replace('mailto:', '') }
+    </Email>
   </Container>
 )
 
 export const query = graphql`
   query IndexRuQuery {
     ru: prismicHomepage(type: {eq: "homepage"}, lang: {eq: "ru"}) {
+      data {
+        title {
+          text
+        }
+        underconstruction {
+          text 
+        }
+        email {
+          url
+        }
+        slider {
+          image {
+            url
+          }
+          caption
+        }
+      }
+    }
+    en: prismicHomepage(type: {eq: "homepage"}, lang: {eq: "en-us"}) {
       data {
         title {
           text
