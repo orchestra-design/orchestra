92c704ce3588c30fda62342a713f751e8a9a6d88
Alexei Accio <alexei.accio@gmail.com>
2018-07-05 20:30:59 +0300

    fixfix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,34 +2,38 @@
 import React, { Fragment } from 'react'
 import Img from 'gatsby-image'
 import { withPrefix } from 'gatsby-link'
-import { lifecycle } from 'recompose'
 import { graphql } from 'gatsby'
 import { css } from 'react-emotion'
 
 import TemplateWrapper from '../components/layouts'
 
-const withLifecicle = lifecycle({
-  state: { lang: 'ru' },
-  componentDidMount() {
-    const language = window.navigator.userLanguage || window.navigator.language
-    const langKey = language.includes('en') ? 'en' : 'ru'
-    const redirectUrl = withPrefix(`/${langKey}/`)
-    window.___history.replace(redirectUrl)
+class RedirectIndex extends React.PureComponent {
+  constructor(args) {
+    super(args)
+
+    // Skip build, Browsers only
+    if (typeof window !== 'undefined') {
+      const language = window.navigator.userLanguage || window.navigator.language
+      const langKey = language.includes('en') ? 'en' : 'ru'
+      const redirectUrl = withPrefix(`/${langKey}/`)
+      window.___history.replace(redirectUrl)
+    }
   }
-})
+  render() {
+    return (
+      <Fragment>
+        <TemplateWrapper site={this.props.data.site} lang="ru"></TemplateWrapper>
+        <Img 
+          resolutions={this.props.data.site.data.siteimage.localFile.childImageSharp.resolutions} 
+          className={css`${tw('pin')};`} 
+          style={{position: 'absolute'}} 
+        />
+      </Fragment>
+    )
+  }
+}
 
-const Index = withLifecicle(({ data: { site } }) => (
-  <Fragment>
-    <TemplateWrapper {...{site}} lang="ru"></TemplateWrapper>
-    <Img 
-      resolutions={site.data.siteimage.localFile.childImageSharp.resolutions} 
-      className={css`${tw('pin')};`} 
-      style={{position: 'absolute'}} 
-    />
-  </Fragment>
-))
-
-export default Index
+export default RedirectIndex
 
 export const query = graphql`
   query IndexQuery {
