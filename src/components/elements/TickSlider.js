4b9ce0397eed75d7d0efb8c1e50a90e77ac7efce
Alexei Accio <alexei.accio@gmail.com>
2018-07-23 16:12:06 +0300

    mobile styles

--- src/components/elements/TickSlider.js
+++ src/components/elements/TickSlider.js
@@ -1,25 +1,37 @@
 /* global tw */
 import React from 'react'
-import styled from 'react-emotion'
+import styled, { css } from 'react-emotion'
 import { compose, lifecycle } from 'recompose'
 import { connect } from 'react-redux'
 
 import { changeTheme, countJumbo } from '../../actions'
 import { camelCase, equals, length, not } from '../../helpers'
-import { ButtonText } from './Typography'
+import { ButtonText, Heading1 } from './Typography'
+import { JustImage } from './JustImage'
 
 const Container = styled('div')`
   ${tw([
-    'flex', 'h-screen'
+    'flex', 'flex-col', 'justify-end', 'screen:h-screen',
+    'mx-auto', 'max-w-desktop', 'relative'
   ])};
-  transition: all .6s ease-in-out;
+  @media(max-width: 599px) {
+    height: 100vw;
+  }
+`
+const Heading =  styled('h1')`
+  ${Heading1};
+  ${tw([
+    'max-w-xs', 'screen:max-w-sm', 'relative',
+    'pb-q48', 'pl-q24', 'screen:p-0', 'screen:mb-q144' 
+  ])};
+  color: ${({theme}) => theme.logoFill};
+  text-shadow: ${({ theme }) => theme.logoShadow && '0 0 1.5rem rgba(0,0,0,0.24)'};
 `
 
 const Caption = styled('div')`
   ${ButtonText}
   ${tw([
-    'invisible', 'screen:visible',
-    'fixed', 'pin-b', 'pin-l',
+    'screen:fixed', 'pin-b', 'pin-l',
     'p-q24', 'z-10'
   ])};
   color: ${({theme}) => theme.color};
@@ -59,8 +71,10 @@ export const TickSlider = enhance(({ changeTheme, image, hiddenMenu, jumboCount,
   
   return (
   <Container>
-  {not(hiddenMenu) &&
-    <Caption>{ image[jumboCount].caption }</Caption>  
-  } 
+    <JustImage className={css`${tw('screen:hidden')}`} image={image[jumboCount].image} />
+    <Heading>{ image[jumboCount].worktitle.text }</Heading>
+    {not(hiddenMenu) &&
+      <Caption>{ image[jumboCount].caption }</Caption>  
+    } 
   </Container>
 )})
--- src/templates/homepage-template.js
+++ src/templates/homepage-template.js
@@ -50,6 +50,9 @@ export const query = graphql`
               }
             }
           }
+          worktitle {
+            text
+          }
           caption
           theme
         }
