783679cef3e3c264b7aa501b8c2b4eaf5e185884
Alexei Accio <alexei.accio@gmail.com>
2018-08-13 10:52:52 +0300

    favicon

--- src/components/elements/SEO.js
+++ src/components/elements/SEO.js
@@ -1,54 +1,66 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from "react";
+import Helmet from "react-helmet";
 
-import defaultImage from '../../assets/default.jpg'
+import defaultImage from "../../assets/default.jpg";
+import favicon from "../../assets/favicon.png";
 
-const getSchemaOrgJSONLD = ({
-  url, seotitle, getImage, description
-}) => [
+const getSchemaOrgJSONLD = ({ url, seotitle, getImage, description }) => [
   {
-    '@context': 'http://schema.org',
-    '@type': 'WebSite',
+    "@context": "http://schema.org",
+    "@type": "WebSite",
     url,
     name: seotitle,
-    alternateName: seotitle,
+    alternateName: seotitle
   },
   {
-    '@context': 'http://schema.org',
-    '@type': 'BreadcrumbList',
+    "@context": "http://schema.org",
+    "@type": "BreadcrumbList",
     itemListElement: [
       {
-        '@type': 'ListItem',
+        "@type": "ListItem",
         position: 1,
         item: {
-          '@id': url,
+          "@id": url,
           name: seotitle,
-          getImage,
-        },
-      },
-    ],
-  },
-]
+          getImage
+        }
+      }
+    ]
+  }
+];
 
-export const SEO = ({ seo: { uid, lang, data: {
-  seotitle, seodescription,
-  seokeywords, seoimage
-}}}) => {
-  const siteUrl = 'https://www.orchestra-design.com/'
-  const fbAppID = ''
-  const url = uid !== null ? `${siteUrl}${lang.replace('-us', '')}/${uid.replace(/.{3}$/i, '')}` : siteUrl
-  const getImage = seoimage && seoimage.localFile ? seoimage.localFile.childImageSharp.resolutions.src : defaultImage
-  const schemaOrgJSONLD = getSchemaOrgJSONLD({    
-    url, seotitle, getImage, seodescription
-  })  
+export const SEO = ({
+  seo: {
+    uid,
+    lang,
+    data: { seotitle, seodescription, seokeywords, seoimage }
+  }
+}) => {
+  const siteUrl = "https://www.orchestra-design.com/";
+  const fbAppID = "";
+  const url =
+    uid !== null
+      ? `${siteUrl}${lang.replace("-us", "")}/${uid.replace(/.{3}$/i, "")}`
+      : siteUrl;
+  const getImage =
+    seoimage && seoimage.localFile
+      ? seoimage.localFile.childImageSharp.resolutions.src
+      : defaultImage;
+  const schemaOrgJSONLD = getSchemaOrgJSONLD({
+    url,
+    seotitle,
+    getImage,
+    seodescription
+  });
 
   return (
-    <Helmet      
+    <Helmet
       defaultTitle="Orchestra Design"
       titleTemplate={`%s | Orchestra Design`}
-      title={seotitle} 
+      title={seotitle}
     >
-      {/* General tags */}      
+      <link rel="icon" type="image/png" href={favicon} />
+      {/* General tags */}
       <meta name="description" content={seodescription} />
       <meta name="keywords" content={seokeywords} />
       <meta name="image" content={getImage} />
@@ -65,5 +77,5 @@ export const SEO = ({ seo: { uid, lang, data: {
       <meta property="og:image" content={getImage} />
       <meta property="fb:app_id" content={fbAppID} />
     </Helmet>
-  )
-}
+  );
+};
